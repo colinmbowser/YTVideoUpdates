9fe45be2677eedc1a8b8d767a867e05a85750593
Colin Bowser <cbowser@byui.edu>
2022-11-10 12:23:12 -0800

    made new files and .getignore

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,22 @@
 fn main() {
     println!("Hello, world!");
+    
+
+
+
+    
 }
 
+
+// trim removes whitespace at beginning and end of string such as the \n
+// parse changes the type example: from string to int
 // let guess: u32 = guess.trim().parse().expect("Please type a number!");
+
+
+
+
+
+// https://www.youtube.com/user/VivaLaDirtLeague 
+// https://www.youtube.com/user/AlecBenjaminMusic 
+// https://www.youtube.com/user/PewDiePie/videos
+// format for the youtube channels
